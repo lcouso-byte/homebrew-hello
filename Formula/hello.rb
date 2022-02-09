835590a6cf3d01212bbc6a670166327f4da34946
Lazaro Couso <lcouso@ufl.edu>
2022-02-09 09:11:43 -0500

    feat(formula): add formula for hello

--- Formula/hello.rb
+++ Formula/hello.rb
@@ -1,7 +1,7 @@
 class Hello < Formula
     desc "A simple greeter"
-    homepage "https://github.com/lcouso-bytes/hello"
-    url "https://github.com/sniptt-official/homebrew-hello/releases/download/v1.0.0/hello-macos-x64.tar.gz"
+    homepage "https://github.com/lcouso-byte/hello"
+    url "https://github.com/lcouso-byte/homebrew-hello/releases/download/v1.0.0/hello-macos-x64.tar.gz"
     sha256 "30410853fe7615968eb09de121ceabda8bb3743b997acca128de4f38c9c2fa86"
     version "1.0.0"
     def install
